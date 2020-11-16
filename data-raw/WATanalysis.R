e938ee28d738ac7840a670c5e627e3dd66a85112
Morten Dall <mortendall1988@gmail.com>
2020-11-16 10:00:18 +0100

    corrected error in design matrix generation to include removal of sample post QC

--- data-raw/WATanalysis.R
+++ data-raw/WATanalysis.R
@@ -3,16 +3,17 @@
 count_matrix <- count_matrix_assembly("Human_RNAseq_counts.xlsx")
 
 setup <- load_metadata("setup_data.xlsx")
+
+
+#Quality_control_plots(count_matrix, setup)
+
+#S42 placebo A look like it is seperaring from the remaining groups. Consider removing it
+setup <- setup %>%
+  dplyr::filter(!ID == "S42")
 design <- Generate_design_matrix(setup)
 
-Quality_control_plots(count_matrix, setup)
-
-#S10 and S42 placebo A look like they are seperaring from the remaining groups. Consider removing them
-setup <- setup %>%
-  dplyr::filter(!ID == "S42"& !ID == "S10")
-
 count_matrix <- count_matrix %>%
-  dplyr::select(-S10,-S42)
+  dplyr::select(-S42)
 
 #select what groups from design yuo wish to compare
 ctrsts <- makeContrasts(
@@ -22,6 +23,8 @@ ctrsts <- makeContrasts(
   NR_vs_placebo_B = NR_B-Placebo_B,
   Interaction = (NR_B - NR_A) - (Placebo_B-Placebo_A),
   levels = design)
+
+all(setup$ID == colnames(count_matrix))
 
 dgeResults <- RNAseq_processing(count_matrix, setup, design, ctrsts)
 write.xlsx(dgeResults, file = here("data/edgeR_afterQC.xlsx"), asTable = TRUE)
